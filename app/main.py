29f4647e8f53e0186a2b8ee0d3fea4f054296d3b
Nasirul Islam <nasirul.islam@northsouth.edu>
2022-01-08 21:46:10 +0600

    add test cases

--- app/main.py
+++ app/main.py
@@ -1,19 +1,26 @@
+import os
 from typing import Optional
 
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 from pydantic import BaseModel
 from wonderwords import RandomWord
 
+from .thumbnail_maker import create
+
+STATIC_DIR = os.environ.get("STATIC_DIR", "/tmp/static")
 
 class Thumbnail(BaseModel):
     url: str
     filename: Optional[str] = None
 
 app = FastAPI()
+app.mount("/static", StaticFiles(directory=STATIC_DIR), name="static")
 
-@app.post('/thumbnail', response_model=Thumbnail)
+@app.post("/thumbnail", response_model=Thumbnail)
 def create_thumbnail(thumbnail: Thumbnail):
     rw = RandomWord()
-    filename = '_'.join(rw.random_words(3, include_parts_of_speech=['nouns', 'adjectives']))
+    filename = '_'.join(rw.random_words(3, include_parts_of_speech=["nouns", "adjectives"]))
+    create(thumbnail.url, filename)
     thumbnail.filename = filename
     return thumbnail
